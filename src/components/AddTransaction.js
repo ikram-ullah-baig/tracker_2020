b35e6380fdf2aa591263b50ed8250bb9f88e56ce
ikram-ullah-baig <ikramullahgmt91@gmail.com>
2021-05-05 12:49:44 +0500

    Update AddTransaction.js

--- src/components/AddTransaction.js
+++ src/components/AddTransaction.js
@@ -36,7 +36,7 @@ export const AddTransaction = () => {
                         Amount <br />
                         (negative - expense, positive - income)
                     </label>
-                    <input type="number" value={amount} onChange={(e) => setAmount(e.target.value)}  
+                    <input type="number" value={amount} onChange={(e) => setAmount(e.target.value)}
                     placeholder="Enter amount..." />
                 </div>
                 <button className="btn">Add transaction</button>
