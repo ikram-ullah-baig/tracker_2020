3a2984ed638f12ce93c99d86d4933711ea3e1e2d
ikram-ullah-baig <ikramullahgmt91@gmail.com>
2021-05-05 12:38:41 +0500

    updated css

--- src/components/Header.js
+++ src/components/Header.js
@@ -3,7 +3,7 @@ import React from 'react'
 export const Header = () => {
     return (
         <h2>
-            Expense Tracker
+            <b>Expense Tracker By Ikram</b>
         </h2>
     )
 }
